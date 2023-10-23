5ec82d578753f0f21271877ef207045ae1102c8d
ashraful1910 <ashraful1910@gmail.com>
2023-10-23 18:47:04 +0600

    updated

--- database/migrations/2021_12_02_054840_create_landing_pages_table.php
+++ database/migrations/2021_12_02_054840_create_landing_pages_table.php
@@ -18,18 +18,44 @@ class CreateLandingPagesTable extends Migration
             $table->string('topbar_logo')->nullable();
             $table->json('topbar_menu_items')->nullable();
             $table->json('social_media_menu_items')->nullable();
+            $table->string('topbar_telephone')->nullable();
+            $table->string('topbar_email')->nullable();
+            $table->json('mega_menu_items')->nullable();
 
-            $table->string('home_top_img')->nullable();
-            $table->string('home_top_img2')->nullable();
-            $table->string('home_top_title')->nullable();
-            $table->string('home_top_text')->nullable();
+            $table->string('home_top_hero_title')->nullable();
+            $table->string('home_top_hero_text')->nullable();
+            $table->string('home_top_hero_video')->nullable();
 
-            $table->json('home_content_items')->nullable();
+            $table->string('home_exploring_title')->nullable();
+            $table->text('home_exploring_text')->nullable();
+            $table->json('home_exploring_items')->nullable();
 
-            $table->json('about_content_items')->nullable();
+            $table->string('home_statistics_total_haors')->nullable();
+            $table->string('home_statistics_total_area')->nullable();
+            $table->string('home_statistics_total_projects')->nullable();
 
-            $table->string('contact_title')->nullable();
-            $table->string('contact_img')->nullable();
+            $table->string('home_featured_haors_title')->nullable();
+            $table->string('home_featured_haors_sub_title')->nullable();
+            $table->json('home_featured_haors_items')->nullable();
+            $table->string('home_featured_haors_view_all_url')->nullable();
+
+            $table->string('home_haor_map_title')->nullable();
+            $table->text('home_haor_map_text')->nullable();
+            $table->json('home_haor_map_items')->nullable();
+
+            $table->string('home_conservation_effects_title')->nullable();
+            $table->text('home_conservation_effects_text')->nullable();
+            $table->json('home_conservation_effects_items')->nullable();
+
+            $table->string('home_summary_report_title')->nullable();
+            $table->string('home_summary_report_sub_title')->nullable();
+            $table->json('home_summary_report_items')->nullable();
+            $table->string('home_summary_report_view_all_url')->nullable();
+
+            $table->string('home_recreation_tourism_title')->nullable();
+            $table->json('home_recreation_tourism_items')->nullable();
+
+            $table->json('home_gallery_items')->nullable();
 
             $table->string('cookie_policy_title')->nullable();
             $table->text('cookie_policy_content')->nullable();
@@ -41,9 +67,10 @@ class CreateLandingPagesTable extends Migration
             $table->text('terms_conditions_content')->nullable();
 
             $table->string('footer_logo')->nullable();
+            $table->string('footer_text')->nullable();
+            $table->string('footer_contact_address')->nullable();
             $table->json('footer_link_items')->nullable();
-            $table->string('footer_copyright_text')->nullable();
-
+            $table->text('footer_copyright_text')->nullable();
 
             $table->timestamps();
         });
